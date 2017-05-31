e55bac4ad598509d810a747c6ba2d99278a04504
helenypan <yue.pan@me.com>
2017-05-31 12:42:07 +0100

    finish first unique char script and used pytest

---
+++ first_unique_char.py
@@ -0,0 +1,24 @@
+'''
+takes a string as input and returns the first non-repeated charcater in the input string. 
+If there are no unique characters return None.
+
+if input is 'a', return a;
+if input is '112233', return None
+if input is 'aabbcdd124', return c
+'''
+
+def first_unique(string):
+	if len(string) == 1:
+		return string[0]
+	else:
+		for i in range(len(string)):
+			if (i == 0 and string[i] != string[i+1]) or \
+			(i == len(string)-1 and string[i] not in string[:i]) or \
+			(string[i] not in string[:i] and string[i] != string[i+1]):
+				return string[i]
+		return None
+
+def test_answer():
+	assert first_unique("s") == "s"
+	assert first_unique("aabbcdd") == "c"
+
